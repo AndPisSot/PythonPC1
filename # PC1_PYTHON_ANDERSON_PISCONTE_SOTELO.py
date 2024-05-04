563622fede935da61fba1cdedd95e8918263ddb0
AndPisSot <168336242+AndPisSot@users.noreply.github.com>
2024-05-04 02:42:59 +0000

    prueba

--- # PC1_PYTHON_ANDERSON_PISCONTE_SOTELO.py
+++ # PC1_PYTHON_ANDERSON_PISCONTE_SOTELO.py
@@ -33,8 +33,7 @@ print('Monto a dejar como propina: ${}'.format(propina))
 # peso de los payasos y muñecas que saldrán en cada paquete a demanda. Cada payaso pesa 112 g y
 # cada muñeca 75 g. Escribir un programa que lea el número de payasos y muñecas vendidos en el
 # último pedido y calcule el peso total del paquete que será enviado.
-
-    
+  
 # Definir los pesos
 peso_payaso = 112
 peso_muñeca = 75
